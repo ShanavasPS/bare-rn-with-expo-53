db48f8c068d99a1214df6a7a75d43991abb9a396
Shanavas Shaji <shanavascruise@gmail.com>
2025-05-02 07:45:34 +0300

    Using sharevalue in react-native-reanimated leades to crash in custom debug config
    Signed-off-by: Shanavas Shaji <shanavascruise@gmail.com>

--- src/modules/test/screens/ScreenA.tsx
+++ src/modules/test/screens/ScreenA.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Button, View} from 'react-native';
+import Animated, {useSharedValue, withSpring} from 'react-native-reanimated';
 
 export interface ScreenAProps {}
 
 export const ScreenA: React.FC<ScreenAProps> = props => {
-  return <View></View>;
+  const width = useSharedValue(100);
+  const handlePress = () => {
+    console.log('pressed button');
+    width.value = withSpring(width.value + 50);
+  };
+
+  return (
+    <View>
+      <Animated.View
+        style={{
+          width,
+          height: 100,
+          backgroundColor: 'violet',
+        }}
+      />
+      <Button onPress={handlePress} title="Click me" />
+    </View>
+  );
 };
